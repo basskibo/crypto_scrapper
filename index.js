c5c7d51b1b71711eb929e5880e88f9d041a4e359
Bojan Jagetic <jageticb@yahoo.com>
2022-01-26 00:20:51 +0100

    fetching crypto live data , prices/ranking

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const helper_methods = require("./src/helper_method");
 const app = express();
 const config = require("./config/config");
 const newspapers = config.newspapers;
-
+const cryptoPriceProvider = config.cryptoPriceProvider;
 const availableProviders = newspapers.map((provider) => provider.name);
 
 const articles = [];
-let NEWS_ALL = [];
+let NEWS_ALL = [],
+  CRYPTO_PRICES = [];
 
 setInterval(async () => {
   console.log("**************************************");
@@ -23,6 +24,9 @@ app.get("/", (req, res) => {
   res.json("Welcome to the Crypto API !");
 });
 
+/**
+ * description Crypto news / NFT
+ */
 app.get("/news", async (req, res) => {
   if (NEWS_ALL.length > 0) {
     console.log("Using cached data");
@@ -58,6 +62,59 @@ app.get("/news/provider/:newspaperId", async (req, res) => {
       res.json(payloadJSON);
     })
     .catch((err) => console.log(err));
+});
+
+/**
+ * description CRYPTO PRICES
+ */
+app.get("/crypto/prices", async (req, res) => {
+  if (CRYPTO_PRICES.length > 0) {
+    console.log("Using cached data");
+    res.json(NEWS_ALL);
+  } else {
+    console.log(
+      `There is no data, fetching from ${cryptoPriceProvider.address}`
+    );
+    const { data } = await axios.get(cryptoPriceProvider.address);
+    const keys = [
+      "rank",
+      "name",
+      "shortName",
+      "marketCap",
+      "price",
+      "circuilatingSupply",
+      "volume",
+      "Volume / Market Cap",
+      "24h%",
+      "7d%",
+    ];
+    const cryptos = [];
+    const $ = cheerio.load(data);
+    const elemSelector = "table > tbody > tr";
+
+    $(elemSelector).each((parentIdx, parentElem) => {
+      let keyIdx = 0;
+      const coin = {};
+      if (parentIdx < 20) {
+        $(parentElem)
+          .children()
+          .each((children, childElem) => {
+            let tdValue = $(childElem).text();
+            if (keyIdx === 1 || keyIdx === 6) {
+            }
+            if (tdValue) {
+              coin[keys[keyIdx]] = tdValue;
+              keyIdx++;
+            }
+          });
+        cryptos.push(coin);
+        console.log(coin);
+      }
+
+      //   console.log(parentElem);
+    });
+    res.json(cryptos);
+  }
 });
 
 app.listen(PORT, () => {
