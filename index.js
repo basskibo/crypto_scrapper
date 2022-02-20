10b96c64b700f033330c42bc71e18e59997ae92b
basskibo <jagetic.bojan@gmail.com>
2022-02-20 14:41:48 +0100

    changing API routers, adding new providers

--- index.js
+++ index.js
@@ -1,183 +1,183 @@
-const PORT = process.env.PORT || 8000;
-const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const helper_methods = require("./src/helper_method");
-const app = express();
-const config = require("./config/config");
-const cryptoData = require("./data/crypto.json");
-const newspapers = config.newspapers;
-const cryptoPriceProvider = config.cryptoPriceProvider;
-const availableProviders = newspapers.map((provider) => provider.name);
+const PORT = process.env.PORT || 8000
+const express = require("express")
+const axios = require("axios")
+const cheerio = require("cheerio")
+const helper_methods = require("./src/helper_method")
+const app = express()
+const config = require("./config/config")
+const cryptoData = require("./data/crypto.json")
+const newspapers = config.newspapers
+const cryptoPriceProvider = config.cryptoPriceProvider
+const availableProviders = newspapers.map((provider) => provider.name)
 
-const articles = [];
+const articles = []
 let NEWS_ALL = [],
-  CRYPTO_PRICES = [];
+	CRYPTO_PRICES = []
 
 setInterval(async () => {
-  console.log("**************************************");
-  console.log("Fetching data .....");
-  console.log("**************************************");
-  NEWS_ALL = await helper_methods.getNewsFromAllProviders(newspapers);
-}, 300 * 1000);
+	console.log("**************************************")
+	console.log("Fetching data .....")
+	console.log("**************************************")
+	NEWS_ALL = await helper_methods.getNewsFromAllProviders(newspapers)
+}, 300 * 1000)
 
 app.get("/", (req, res) => {
-  res.json("Welcome to the Crypto API !");
-});
+	res.json("Welcome to the Crypto API !")
+})
 
 /**
  * description Crypto news / NFT
  */
 app.get("/news", async (req, res) => {
-  if (NEWS_ALL.length > 0) {
-    console.log("Using cached data");
-    res.json(NEWS_ALL);
-  } else {
-    NEWS_ALL = await helper_methods.getNewsFromAllProviders(newspapers);
-    console.log(
-      `Fetch completed, successfully scrapped data from ${NEWS_ALL.length} articles`
-    );
-    res.json(NEWS_ALL);
-  }
-});
+	if (NEWS_ALL.length > 0) {
+		console.log("Using cached data")
+		res.json(NEWS_ALL)
+	} else {
+		NEWS_ALL = await helper_methods.getNewsFromAllProviders(newspapers, "Bitcoin")
+		console.log(
+			`Fetch completed, successfully scrapped data from ${NEWS_ALL.length} articles`
+		)
+		res.json(NEWS_ALL)
+	}
+})
 
 app.get("/news/provider/:newspaperId", async (req, res) => {
-  const newsPaperId = req.params.newspaperId;
-  const newspaperProvider = newspapers.filter(
-    (newspaper) => newspaper.name == newsPaperId
-  )[0];
-  if (!newspaperProvider) {
-    console.log("ERROR!!!");
-    return res.send(
-      `Provider with name ${req.params.newspaperId} was not found. Available providers are : ${availableProviders}`
-    );
-  }
-  console.log(newspaperProvider);
-  axios
-    .get(newspaperProvider.address)
-    .then((response) => {
-      const payloadJSON = helper_methods.generateArticlePayload(
-        response,
-        newspaperProvider
-      );
-      res.json(payloadJSON);
-    })
-    .catch((err) => console.log(err));
-});
+	const newsPaperId = req.params.newspaperId
+	const newspaperProvider = newspapers.filter(
+		(newspaper) => newspaper.name == newsPaperId
+	)[0]
+	if (!newspaperProvider) {
+		console.log("ERROR!!!")
+
+		return res
+			.status(404)
+			.send(
+				`Provider with name ${req.params.newspaperId} was not found. Available providers are : ${availableProviders}`
+			)
+	}
+	console.log(newspaperProvider)
+	axios
+		.get(newspaperProvider.address)
+		.then((response) => {
+			const payloadJSON = helper_methods.generateArticlePayload(
+				response,
+				newspaperProvider
+			)
+			res.json(payloadJSON)
+		})
+		.catch((err) => console.log(err))
+})
 
 /**
  * description CRYPTO PRICES
  */
 app.get("/crypto/price", async (req, res) => {
-  if (CRYPTO_PRICES.length > 0) {
-    console.log("Using cached data");
-    res.json(NEWS_ALL);
-  } else {
-    const keys = config.cryptoKeys;
-    console.log(
-      `There is no data, fetching from ${cryptoPriceProvider.address}`
-    );
-    const { data } = await axios.get(cryptoPriceProvider.address);
+	if (CRYPTO_PRICES.length > 0) {
+		console.log("Using cached data")
+		res.json(NEWS_ALL)
+	} else {
+		const keys = config.cryptoKeys
+		console.log(`There is no data, fetching from ${cryptoPriceProvider.address}`)
+		const { data } = await axios.get(cryptoPriceProvider.address)
 
-    const cryptos = [];
-    const $ = cheerio.load(data);
-    const elemSelector = "table > tbody > tr";
+		const cryptos = []
+		const $ = cheerio.load(data)
+		const elemSelector = "table > tbody > tr"
 
-    $(elemSelector).each((parentIdx, parentElem) => {
-      let keyIdx = 0;
-      const coin = {};
-      if (parentIdx < 20) {
-        $(parentElem)
-          .children()
-          .each((children, childElem) => {
-            let tdValue = $(childElem).text();
-            if (keyIdx === 1 || keyIdx === 6) {
-            }
-            if (tdValue) {
-              coin[keys[keyIdx]] = tdValue;
-              keyIdx++;
-            }
-          });
-        coin.name = helper_methods.sliceCryptoName(coin);
-        coin.circuilatingSupply =
-          helper_methods.sliceCryptocircuilatingSupply(coin);
-        coin.marketCap = helper_methods.sliceMarketCap(coin);
-        cryptos.push(coin);
-        console.log(coin);
-      }
-    });
-    res.json(cryptos);
-  }
-});
+		$(elemSelector).each((parentIdx, parentElem) => {
+			let keyIdx = 0
+			const coin = {}
+			if (parentIdx < 20) {
+				$(parentElem)
+					.children()
+					.each((children, childElem) => {
+						let tdValue = $(childElem).text()
+						if (keyIdx === 1 || keyIdx === 6) {
+						}
+						if (tdValue) {
+							coin[keys[keyIdx]] = tdValue
+							keyIdx++
+						}
+					})
+				coin.name = helper_methods.sliceCryptoName(coin)
+				coin.circuilatingSupply = helper_methods.sliceCryptocircuilatingSupply(coin)
+				coin.marketCap = helper_methods.sliceMarketCap(coin)
+				cryptos.push(coin)
+				console.log(coin)
+			}
+		})
+		res.json(cryptos)
+	}
+})
 
-app.get("/crypto/top", (req, res) => {
-  res.json(cryptoData.top50Currencies);
-});
+app.get("/crypto/top50", (req, res) => {
+	res.json(cryptoData.top50Currencies)
+})
 
-app.get("/crypto/all", async (req, res) => {
-  const keys = config.cryptoKeys;
-  console.log(`There is no data, fetching from ${cryptoPriceProvider.address}`);
-  const { data } = await axios.get(cryptoPriceProvider.address);
+app.get("/crypto/list", async (req, res) => {
+	const keys = config.cryptoKeys
+	console.log(`There is no data, fetching from ${cryptoPriceProvider.address}`)
+	const { data } = await axios.get(cryptoPriceProvider.address)
 
-  const cryptos = [];
-  const $ = cheerio.load(data);
-  const elemSelector = "table > tbody > tr";
+	const cryptos = []
+	const $ = cheerio.load(data)
+	const elemSelector = "table > tbody > tr"
 
-  $(elemSelector).each((parentIdx, parentElem) => {
-    let keyIdx = 0;
-    const coin = {};
-    if (parentIdx < 20) {
-      $(parentElem)
-        .children()
-        .each((children, childElem) => {
-          let tdValue = $(childElem).text();
-          if (keyIdx === 1 || keyIdx === 2) {
-            coin[keys[keyIdx]] = tdValue;
-          }
-          keyIdx++;
-        });
-      coin.name = helper_methods.sliceCryptoName(coin);
+	$(elemSelector).each((parentIdx, parentElem) => {
+		let keyIdx = 0
+		const coin = {}
+		if (parentIdx < 20) {
+			$(parentElem)
+				.children()
+				.each((children, childElem) => {
+					let tdValue = $(childElem).text()
+					if (keyIdx === 1 || keyIdx === 2) {
+						coin[keys[keyIdx]] = tdValue
+					}
+					keyIdx++
+				})
+			coin.name = helper_methods.sliceCryptoName(coin)
 
-      cryptos.push(coin);
-      console.log(coin);
-    }
-  });
-  res.json(cryptos);
-});
+			cryptos.push(coin)
+			console.log(coin)
+		}
+	})
+	res.json(cryptos)
+})
 
-app.get("/crypto/get/:cryptoName", async (req, res) => {
-  const keys = config.cryptoKeys;
-  const cryptoName = req.params.cryptoName;
-  console.log(`There is no data, fetching from ${cryptoPriceProvider.address}`);
-  const { data } = await axios.get(cryptoPriceProvider.address);
+app.get("/crypto/:cryptoName", async (req, res) => {
+	const keys = config.cryptoKeys
+	const cryptoName = req.params.cryptoName
+	console.log(`There is no data, fetching from ${cryptoPriceProvider.address}`)
+	const { data } = await axios.get(cryptoPriceProvider.address)
 
-  const cryptos = [];
-  const $ = cheerio.load(data);
-  const elemSelector = "table > tbody > tr";
+	const cryptos = []
+	const $ = cheerio.load(data)
+	const elemSelector = "table > tbody > tr"
 
-  $(elemSelector).each((parentIdx, parentElem) => {
-    let keyIdx = 0;
-    const coin = {};
-    if (parentIdx < 20) {
-      $(parentElem)
-        .children()
-        .each((children, childElem) => {
-          let tdValue = $(childElem).text();
-          if (tdValue) {
-            coin[keys[keyIdx]] = tdValue;
-            keyIdx++;
-          }
-        });
-      coin.name = helper_methods.sliceCryptoName(coin);
-      if (cryptoName === coin.name || cryptoName === coin.shortName) {
-        cryptos.push(coin);
-        console.log(coin);
-      }
-    }
-  });
-  res.json(cryptos);
-});
+	$(elemSelector).each((parentIdx, parentElem) => {
+		let keyIdx = 0
+		const coin = {}
+		if (parentIdx < 20) {
+			$(parentElem)
+				.children()
+				.each((children, childElem) => {
+					let tdValue = $(childElem).text()
+					if (tdValue) {
+						coin[keys[keyIdx]] = tdValue
+						keyIdx++
+					}
+				})
+			coin.name = helper_methods.sliceCryptoName(coin)
+			if (cryptoName === coin.name || cryptoName === coin.shortName) {
+				cryptos.push(coin)
+				console.log(coin)
+			}
+		}
+	})
+	res.json(cryptos)
+})
 
 app.listen(PORT, () => {
-  console.log("Server listening on port: ", PORT);
-});
+	console.log("Server listening on port: ", PORT)
+})
--- src/helper_method.js
+++ src/helper_method.js
@@ -1,95 +1,96 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+const cheerio = require("cheerio")
+const axios = require("axios")
 
 const removeDuplicates = (originalArray, prop) => {
-  var newArray = [];
-  var lookupObject = {};
+	var newArray = []
+	var lookupObject = {}
 
-  for (var i in originalArray) {
-    lookupObject[originalArray[i][prop]] = originalArray[i];
-  }
+	for (var i in originalArray) {
+		lookupObject[originalArray[i][prop]] = originalArray[i]
+	}
 
-  for (i in lookupObject) {
-    newArray.push(lookupObject[i]);
-  }
-  return newArray;
-};
+	for (i in lookupObject) {
+		newArray.push(lookupObject[i])
+	}
+	return newArray
+}
 
 const generateArticlePayload = (response, newspaperAddr) => {
-  const html = response.data;
-  const $ = cheerio.load(html);
-  let articles = [];
-  $('a:contains("NFT")', html).each(function () {
-    // title parsing and removing whitespaces
-    let title = $(this).text();
-    title = title.replace(/^\s+|\s+$/gm, "");
-    title = title.replace(/(\r\n|\n|\r)/gm, "");
-    // URL generation and formatting
-    let url = $(this).attr("href");
-    let parsedUrl = `${newspaperAddr.address}${url}`;
-    let formattedUrl = newspaperAddr.urlRootFormation
-      ? `${newspaperAddr.urlRootFormation}${url}`
-      : parsedUrl;
+	const html = response.data
+	const $ = cheerio.load(html)
+	let articles = []
+	$('a:contains("NFT")', html).each(function () {
+		// title parsing and removing whitespaces
+		let title = $(this).text()
+		title = title.replace(/^\s+|\s+$/gm, "")
+		title = title.replace(/(\r\n|\n|\r)/gm, "")
+		// URL generation and formatting
+		let url = $(this).attr("href")
+		let parsedUrl = `${newspaperAddr.address}${url}`
+		let formattedUrl = newspaperAddr.urlRootFormation
+			? `${newspaperAddr.urlRootFormation}${url}`
+			: parsedUrl
 
-    if (newspaperAddr.baseUrl) {
-      formattedUrl = url;
-    }
+		if (newspaperAddr.baseUrl) {
+			formattedUrl = url
+		}
 
-    title = title.replace(/^\s+|\s+$/gm, "");
-    title = title.replace(/(\r\n|\n|\r)/gm, "");
-    // URL generation and formatting
-    console.log(title);
-    articles.push({
-      title,
-      url: formattedUrl,
-      source: newspaperAddr.name,
-      provider: newspaperAddr.provider,
-    });
-  });
-  articles = removeDuplicates(articles, "url");
+		title = title.replace(/^\s+|\s+$/gm, "")
+		title = title.replace(/(\r\n|\n|\r)/gm, "")
+		// URL generation and formatting
+		console.log(title)
+		articles.push({
+			title,
+			url: formattedUrl,
+			source: newspaperAddr.name,
+			provider: newspaperAddr.provider,
+		})
+	})
+	articles = removeDuplicates(articles, "url")
 
-  return articles;
-};
+	return articles
+}
 
-const getNewsFromAllProviders = async (newspapers) => {
-  let articles = [];
+const getNewsFromAllProviders = async (newspapers, keyword) => {
+	let articles = []
+	console.log("fetching news for ", keyword)
+	for (let i = 0; i < newspapers.length; i++) {
+		const source = newspapers[i]
+		await axios.get(source.address).then((response) => {
+			const html = response.data
+			const $ = cheerio.load(html)
+			const searchQuery = source.textSource
+				? `a:contains("${source.textLocation}")`
+				: `a:contains(${JSON.stringify(keyword)})`
 
-  for (let i = 0; i < newspapers.length; i++) {
-    const source = newspapers[i];
-    await axios.get(source.address).then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const searchQuery = source.textSource
-        ? `a:contains("${source.textLocation}")`
-        : 'a:contains("NFT")';
-      //   console.log(searchQuery);
-      $(searchQuery, html).each(function () {
-        // title parsing and removing whitespaces
-        let title = source.textLocation
-          ? $(this).attr(source.textLocation)
-          : $(this).text();
-        title = title.replace(/^\s+|\s+$/gm, "");
-        title = title.replace(/(\r\n|\n|\r)/gm, "");
-        // URL generation and formatting
-        const url = $(this).attr("href");
-        const parsedSource = `${source.address}${url}`;
-        const formattedUrl = source.urlRootFormation
-          ? `${source.urlRootFormation}${url}`
-          : parsedSource;
+			console.log(searchQuery)
+			$(searchQuery, html).each(function () {
+				// title parsing and removing whitespaces
+				let title = source.textLocation
+					? $(this).attr(source.textLocation)
+					: $(this).text()
+				title = title.replace(/^\s+|\s+$/gm, "")
+				title = title.replace(/(\r\n|\n|\r)/gm, "")
+				// URL generation and formatting
+				const url = $(this).attr("href")
+				const parsedSource = `${source.address}${url}`
+				const formattedUrl = source.urlRootFormation
+					? `${source.urlRootFormation}${url}`
+					: parsedSource
 
-        articles.push({
-          title,
-          url: formattedUrl,
-          source: source.name,
-          provider: source.provider,
-        });
-        console.log("PUSHING >>>>> ", title);
-        articles = removeDuplicates(articles, "url");
-      });
-    });
-  }
-  return articles;
-};
+				articles.push({
+					title,
+					url: formattedUrl,
+					source: source.name,
+					provider: source.provider,
+				})
+				console.log("PUSHING >>>>> ", title)
+				articles = removeDuplicates(articles, "url")
+			})
+		})
+	}
+	return articles
+}
 
 /**
  * description Get crypto information and prices
@@ -97,55 +98,55 @@ const getNewsFromAllProviders = async (newspapers) => {
  * @params {Object} response - response from cheerio
  */
 const getCryptoPrices = async (provider, response) => {
-  const html = response.data;
-  const $ = cheerio.load(html);
-  let articles = [];
-  const elemSelector =
-    "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
-  $(elemSelector).each((parentIdx, parentElem) => {
-    console.log(parentIdx);
-  });
-};
+	const html = response.data
+	const $ = cheerio.load(html)
+	let articles = []
+	const elemSelector =
+		"#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr"
+	$(elemSelector).each((parentIdx, parentElem) => {
+		console.log(parentIdx)
+	})
+}
 
 const sliceCryptoName = (coin) => {
-  try {
-    console.log(coin);
-    const fullname = coin.name;
-    const shortLength = coin.shortName.length;
-    console.log(fullname.substring(shortLength));
-    return fullname.substring(shortLength);
-  } catch (exc) {
-    console.log("there was error slicing crypto name...");
-  }
-};
+	try {
+		console.log(coin)
+		const fullname = coin.name
+		const shortLength = coin.shortName.length
+		console.log(fullname.substring(shortLength))
+		return fullname.substring(shortLength)
+	} catch (exc) {
+		console.log("there was error slicing crypto name...")
+	}
+}
 
 const sliceCryptocircuilatingSupply = (coin) => {
-  try {
-    const circ = coin.circuilatingSupply;
-    const newStr = circ.split(" ");
-    const circulatingParsed = `${newStr[0]} ${coin.shortName}`;
-    return circulatingParsed;
-  } catch (exc) {
-    console.log("there was error sliceCryptocircuilatingSupply...");
-  }
-};
+	try {
+		const circ = coin.circuilatingSupply
+		const newStr = circ.split(" ")
+		const circulatingParsed = `${newStr[0]} ${coin.shortName}`
+		return circulatingParsed
+	} catch (exc) {
+		console.log("there was error sliceCryptocircuilatingSupply...")
+	}
+}
 
 const sliceMarketCap = (coin) => {
-  try {
-    const circ = coin.marketCap;
-    const newStr = circ.split("$");
-    console.log(newStr);
-    return newStr[2];
-  } catch (exc) {
-    console.log("there was error sliceMarketCap...");
-  }
-};
+	try {
+		const circ = coin.marketCap
+		const newStr = circ.split("$")
+		console.log(newStr)
+		return newStr[2]
+	} catch (exc) {
+		console.log("there was error sliceMarketCap...")
+	}
+}
 
 module.exports = {
-  generateArticlePayload: generateArticlePayload,
-  getNewsFromAllProviders: getNewsFromAllProviders,
-  getCryptoPrices: getCryptoPrices,
-  sliceCryptoName: sliceCryptoName,
-  sliceCryptocircuilatingSupply: sliceCryptocircuilatingSupply,
-  sliceMarketCap: sliceMarketCap,
-};
+	generateArticlePayload: generateArticlePayload,
+	getNewsFromAllProviders: getNewsFromAllProviders,
+	getCryptoPrices: getCryptoPrices,
+	sliceCryptoName: sliceCryptoName,
+	sliceCryptocircuilatingSupply: sliceCryptocircuilatingSupply,
+	sliceMarketCap: sliceMarketCap,
+}
